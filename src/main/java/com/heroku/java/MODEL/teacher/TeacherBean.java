6d87c27080ded1b92502fbdc3f2c23b15e70bbfc
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-15 00:47:55 +0800

    adding teacherBean

---
+++ src/main/java/com/heroku/java/MODEL/teacher/TeacherBean.java
@@ -0,0 +1,159 @@
+package com.heroku.java.MODEL.teacher;
+
+public class TeacherBean {
+    private String teacherID;
+	private String teacherUsername;
+	private String teacherName;
+	private String teacherEmail;
+	private String teacherPhone;
+	private String teacherGender;
+	private String teacherRole;
+	private String teacherAddress;
+	private String teacherPassword;
+	private String teacherDOB;
+	
+	public TeacherBean() {
+		
+	}
+
+    /**
+     * @return String return the teacherID
+     */
+    public String getTeacherID() {
+        return teacherID;
+    }
+
+    /**
+     * @param teacherID the teacherID to set
+     */
+    public void setTeacherID(String teacherID) {
+        this.teacherID = teacherID;
+    }
+
+    /**
+     * @return String return the teacherUsername
+     */
+    public String getTeacherUsername() {
+        return teacherUsername;
+    }
+
+    /**
+     * @param teacherUsername the teacherUsername to set
+     */
+    public void setTeacherUsername(String teacherUsername) {
+        this.teacherUsername = teacherUsername;
+    }
+
+    /**
+     * @return String return the teacherName
+     */
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    /**
+     * @param teacherName the teacherName to set
+     */
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    /**
+     * @return String return the teacherEmail
+     */
+    public String getTeacherEmail() {
+        return teacherEmail;
+    }
+
+    /**
+     * @param teacherEmail the teacherEmail to set
+     */
+    public void setTeacherEmail(String teacherEmail) {
+        this.teacherEmail = teacherEmail;
+    }
+
+    /**
+     * @return String return the teacherPhone
+     */
+    public String getTeacherPhone() {
+        return teacherPhone;
+    }
+
+    /**
+     * @param teacherPhone the teacherPhone to set
+     */
+    public void setTeacherPhone(String teacherPhone) {
+        this.teacherPhone = teacherPhone;
+    }
+
+    /**
+     * @return String return the teacherGender
+     */
+    public String getTeacherGender() {
+        return teacherGender;
+    }
+
+    /**
+     * @param teacherGender the teacherGender to set
+     */
+    public void setTeacherGender(String teacherGender) {
+        this.teacherGender = teacherGender;
+    }
+
+    /**
+     * @return String return the teacherRole
+     */
+    public String getTeacherRole() {
+        return teacherRole;
+    }
+
+    /**
+     * @param teacherRole the teacherRole to set
+     */
+    public void setTeacherRole(String teacherRole) {
+        this.teacherRole = teacherRole;
+    }
+
+    /**
+     * @return String return the teacherAddress
+     */
+    public String getTeacherAddress() {
+        return teacherAddress;
+    }
+
+    /**
+     * @param teacherAddress the teacherAddress to set
+     */
+    public void setTeacherAddress(String teacherAddress) {
+        this.teacherAddress = teacherAddress;
+    }
+
+    /**
+     * @return String return the teacherPassword
+     */
+    public String getTeacherPassword() {
+        return teacherPassword;
+    }
+
+    /**
+     * @param teacherPassword the teacherPassword to set
+     */
+    public void setTeacherPassword(String teacherPassword) {
+        this.teacherPassword = teacherPassword;
+    }
+
+    /**
+     * @return String return the teacherDOB
+     */
+    public String getTeacherDOB() {
+        return teacherDOB;
+    }
+
+    /**
+     * @param teacherDOB the teacherDOB to set
+     */
+    public void setTeacherDOB(String teacherDOB) {
+        this.teacherDOB = teacherDOB;
+    }
+
+}
