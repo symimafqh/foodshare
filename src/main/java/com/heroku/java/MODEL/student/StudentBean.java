868e1e7f08089cb3ba2986cf7059ce26a138791b
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-14 17:50:45 +0800

    studentBean

---
+++ src/main/java/com/heroku/java/MODEL/student/StudentBean.java
@@ -0,0 +1,144 @@
+package com.heroku.java.MODEL.student;
+
+public class StudentBean {
+    private String studentIC;
+	private String studentName;
+	private String studentEmail;
+	private String studentPhone;
+	private String studentDOB;
+    private String studentGender;
+    private String studentClass;
+    private String studentPassword;
+    private String studentAddress;
+
+    public StudentBean(){
+    	
+    }
+
+    /**
+     * @return String return the studentIC
+     */
+    public String getStudentIC() {
+        return studentIC;
+    }
+
+    /**
+     * @param studentIC the studentIC to set
+     */
+    public void setStudentIC(String studentIC) {
+        this.studentIC = studentIC;
+    }
+
+    /**
+     * @return String return the studentName
+     */
+    public String getStudentName() {
+        return studentName;
+    }
+
+    /**
+     * @param studentName the studentName to set
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    /**
+     * @return String return the studentEmail
+     */
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    /**
+     * @param studentEmail the studentEmail to set
+     */
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    /**
+     * @return String return the studentPhone
+     */
+    public String getStudentPhone() {
+        return studentPhone;
+    }
+
+    /**
+     * @param studentPhone the studentPhone to set
+     */
+    public void setStudentPhone(String studentPhone) {
+        this.studentPhone = studentPhone;
+    }
+
+    /**
+     * @return String return the studentDOB
+     */
+    public String getStudentDOB() {
+        return studentDOB;
+    }
+
+    /**
+     * @param studentDOB the studentDOB to set
+     */
+    public void setStudentDOB(String studentDOB) {
+        this.studentDOB = studentDOB;
+    }
+
+    /**
+     * @return String return the studentGender
+     */
+    public String getStudentGender() {
+        return studentGender;
+    }
+
+    /**
+     * @param studentGender the studentGender to set
+     */
+    public void setStudentGender(String studentGender) {
+        this.studentGender = studentGender;
+    }
+
+    /**
+     * @return String return the studentClass
+     */
+    public String getStudentClass() {
+        return studentClass;
+    }
+
+    /**
+     * @param studentClass the studentClass to set
+     */
+    public void setStudentClass(String studentClass) {
+        this.studentClass = studentClass;
+    }
+
+    /**
+     * @return String return the studentPassword
+     */
+    public String getStudentPassword() {
+        return studentPassword;
+    }
+
+    /**
+     * @param studentPassword the studentPassword to set
+     */
+    public void setStudentPassword(String studentPassword) {
+        this.studentPassword = studentPassword;
+    }
+
+    /**
+     * @return String return the studentAddress
+     */
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    /**
+     * @param studentAddress the studentAddress to set
+     */
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+}
