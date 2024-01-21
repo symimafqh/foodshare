72465ddffb009595d95e08e0540bb79f086d5e40
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-21 21:19:10 +0800

    teacher login process and dashboard and side bar controller

---
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerTeacher.java
@@ -0,0 +1,15 @@
+package com.heroku.java.CONTROLLER.sidebar;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+@Controller
+public class SideBarControllerTeacher {
+
+    @GetMapping("/dashboardTeacher")
+      public String index1(@RequestParam(name = "success", required = false) Boolean success, HttpSession session) {
+        String teacherUsername = (String) session.getAttribute("teacherUsername");
+        return "teacher/dashboardTeacher";
+    }
+}
