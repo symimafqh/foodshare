97e65cd8a90dc59df47729b4cf1b74489fea85d4
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-22 00:11:27 +0800

    tukat getmapping kat GettingStartedApplication

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerTeacher.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerTeacher.java
@@ -13,9 +13,5 @@ public class SideBarControllerTeacher {
         return "teacher/dashboardTeacher";
     }
 
-    @GetMapping("/profileTeacher_edit")
-    public String editProfileTeacher(HttpSession session) {
-        String teacherUsername = (String) session.getAttribute("teacherUsername");
-        return "teacher/profileTeacher/profileTeacher_edit";
-    }
+  
 }
