640e4bac0aef67bbbe9a3197587441ff77652289
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-22 13:09:46 +0800

    modify sidebar stduent

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarStudentController.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarStudentController.java
@@ -1,13 +1,35 @@
 package com.heroku.java.CONTROLLER.sidebar;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.heroku.java.MODEL.student.StudentBean;
 
 import jakarta.servlet.http.HttpSession;
 
+import java.sql.*;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.util.List;
+
 @Controller
 public class SideBarStudentController {
+     private final DataSource dataSource;
+
+      @Autowired
+    public SideBarStudentController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
      @GetMapping("/dashboardStudent")
       public String index1(@RequestParam(name = "success", required = false) Boolean success, HttpSession session) {
         String studentIC = (String) session.getAttribute("studentIC");
@@ -15,8 +37,44 @@ public class SideBarStudentController {
     }
 
     @GetMapping("/edit_profile")
-    public String editProfile(HttpSession session) {
+    public String editProfile(HttpSession session, Model model) {
         String studentIC = (String) session.getAttribute("studentIC");
+        System.out.println("IC Number : " + studentIC);
+        try {
+            Connection connection = dataSource.getConnection();
+            String sql = "SELECT guesticnumber, guestname, guestphonenumber, guestgender, guestreligion, guestrace, guestaddress, guestemail, guestpassword FROM public.guest WHERE guesticnumber LIKE ?";
+            final var statement = connection.prepareStatement(sql);
+            statement.setString(1, studentIC);
+            final var resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String password = resultSet.getString("studentPassword");
+                String studentName = resultSet.getString("studentName");
+                String studentEmail = resultSet.getString("studentEmail");
+                String studentPhone = resultSet.getString("studentPhone");
+                String studentDOB = resultSet.getString("studentDOB");
+                String studentGender = resultSet.getString("studentGender");
+               String studentClass = resultSet.getString("studentClass");
+               String studentAddress = resultSet.getString("studentAddress");
+
+                StudentBean s = new StudentBean();
+                s.setStudentIC(studentIC);
+                s.setStudentPassword(password);
+                s.setStudentName(studentName);
+                s.setStudentEmail(studentEmail);
+                s.setStudentPhone(studentPhone);
+                s.setStudentDOB(studentDOB);
+                s.setStudentGender(studentGender);
+                s.setStudentClass(studentClass);
+                s.setStudentAddress(studentAddress);
+                model.addAttribute("s", s);
+
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "student/profile/edit_profile";
     }
 
