a2682b42206105e185ed73a7f85abbab2ba464e6
AinurAsyirah <2023388507@student.uitm.edu.my>
2024-01-23 12:19:25 +0800

    registration bean and view page.
    dashboard student mapping to registration page
    profile admin change input type for password

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarStudentController.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarStudentController.java
@@ -80,14 +80,47 @@ public class SideBarStudentController {
         return "student/profile/edit_profile";
     }
 
-    @GetMapping("/semasa")
-    public String semasa() {
-        return "semasa";
-    }
 
-    @GetMapping("/pendaftaran")
-    public String pendaftaran() {
-        return "pendaftaran";
+    @GetMapping("/registration")
+    public String registration(@RequestParam(name = "success", required = false) Boolean success, HttpSession session, Model model) {
+        String studentIC = (String) session.getAttribute("studentIC");
+        try {
+            Connection connection = dataSource.getConnection();
+            String sql = "SELECT * FROM public.student where studentic=?";
+            final var statement = connection.prepareStatement(sql);
+            statement.setString(1, studentIC);
+            final var resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String studentName = resultSet.getString("studentName");
+                String studentEmail = resultSet.getString("studentEmail");
+                String studentPhone = resultSet.getString("studentPhone");
+                String studentDOB = resultSet.getString("studentDOB");
+                String studentGender = resultSet.getString("studentGender");
+                String studentClass = resultSet.getString("studentClass");
+                String studentAddress = resultSet.getString("studentAddress");
+                String studentPassword = resultSet.getString("studentPassword");
+
+                StudentBean s = new StudentBean();
+
+                s.setStudentIC(studentIC);
+                s.setStudentName(studentName);
+                s.setStudentEmail(studentEmail);
+                s.setStudentPhone(studentPhone);
+                s.setStudentDOB(studentDOB);
+                s.setStudentGender(studentGender);
+                s.setStudentClass(studentClass);
+                s.setStudentAddress(studentAddress);
+                s.setStudentPassword(studentPassword);
+
+                model.addAttribute("s", s);
+
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "student/registration/registration";
     }
 
     @GetMapping("/semakan")
---
+++ src/main/java/com/heroku/java/MODEL/registration/RegistrationBean.java
@@ -0,0 +1,105 @@
+package com.heroku.java.MODEL.registration;
+
+import com.heroku.java.MODEL.activity.ActivityBean;
+import com.heroku.java.MODEL.student.StudentBean;
+
+public class RegistrationBean {
+
+    private int registrationid;
+    private String studentic;
+    private String activityid;
+    private int unitReg;
+	private int clubReg;
+	private int sportReg;
+    private ActivityBean activity;
+
+    public RegistrationBean(){
+
+    }
+
+    /**
+     * @return int return the registrationid
+     */
+    public int getRegistrationid() {
+        return registrationid;
+    }
+
+    /**
+     * @param registrationid the registrationid to set
+     */
+    public void setRegistrationid(int registrationid) {
+        this.registrationid = registrationid;
+    }
+
+    /**
+     * @return String return the studentic
+     */
+    public String getStudentic() {
+        return studentic;
+    }
+
+    /**
+     * @param studentic the studentic to set
+     */
+    public void setStudentic(String studentic) {
+        this.studentic = studentic;
+    }
+
+    /**
+     * @return String return the activityid
+     */
+    public String getActivityid() {
+        return activityid;
+    }
+
+    /**
+     * @param activityid the activityid to set
+     */
+    public void setActivityid(String activityid) {
+        this.activityid = activityid;
+    }
+
+
+    /**
+     * @return int return the unitReg
+     */
+    public int getUnitReg() {
+        return unitReg;
+    }
+
+    /**
+     * @param unitReg the unitReg to set
+     */
+    public void setUnitReg(int unitReg) {
+        this.unitReg = unitReg;
+    }
+
+    /**
+     * @return int return the clubReg
+     */
+    public int getClubReg() {
+        return clubReg;
+    }
+
+    /**
+     * @param clubReg the clubReg to set
+     */
+    public void setClubReg(int clubReg) {
+        this.clubReg = clubReg;
+    }
+
+    /**
+     * @return int return the sportReg
+     */
+    public int getSportReg() {
+        return sportReg;
+    }
+
+    /**
+     * @param sportReg the sportReg to set
+     */
+    public void setSportReg(int sportReg) {
+        this.sportReg = sportReg;
+    }
+
+}
