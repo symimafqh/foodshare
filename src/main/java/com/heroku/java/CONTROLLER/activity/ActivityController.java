6f1624d1f6f386ca3c262f89b2077181d9ae06dd
symimafqh <2023380125@student.uitm.edu.my>
2024-01-24 17:57:52 +0800

    update post

--- src/main/java/com/heroku/java/CONTROLLER/activity/ActivityController.java
+++ src/main/java/com/heroku/java/CONTROLLER/activity/ActivityController.java
@@ -375,28 +375,30 @@ public String UpdateSukan( @RequestParam("activityID") int activityid,Model mode
 
 
     @PostMapping("/UpdateSukan")
-    public String UpdateSukan(@RequestParam int activityId, @RequestParam String activityName, @RequestParam String info, @RequestParam Integer quota, ActivityBean ab, SukanBean sb, Model model) {
+    public String UpdateSukan(@RequestParam("activityID") int activityid, @RequestParam("namaSukan") String activityName, @RequestParam("info") String info, @RequestParam("quota") Integer quota, ActivityBean ab, SukanBean sb, Model model) {
         
-        int activity = activityId;
-        String unitName = activityName;
+        //int activity = activityId;
+        //String unitName = activityName;
 
-        System.out.println(activity + unitName);
-
+        System.out.println(activityid);
+        System.out.println(activityName);
+        System.out.println(info);
+        System.out.println(quota);
 	    try {
             Connection con = dataSource.getConnection();
 	        String sql1 = "UPDATE ACTIVITY SET ACTIVITYNAME = ? WHERE ACTIVITYID = ?";
 	        try (PreparedStatement ps1 = con.prepareStatement(sql1)) {
-	            ps1.setString(1, ab.getActivityName());
-	            ps1.setInt(2, activity);
+	            ps1.setString(1, activityName);
+	            ps1.setInt(2, activityid);
 	            ps1.executeUpdate();
-	            System.out.println("Successfully updated activity table"+activityId);
+	            System.out.println("Successfully updated activity table "+activityid);
 	        }
 
 	        String sql2 = "UPDATE SPORT SET SPORTINFORMATION=?, SPORTQUOTA=?  WHERE ACTIVITYID = ?";
 	        try (PreparedStatement ps2 = con.prepareStatement(sql2)) {
-	            ps2.setString(1, sb.getInfoSukan());
-	            ps2.setInt(2, sb.getQuotaSukan()); // Assuming getQuotaSukan() returns an integer
-	            ps2.setInt(3, activityId);
+	            ps2.setString(1, info);
+	            ps2.setInt(2, quota); // Assuming getQuotaSukan() returns an integer
+	            ps2.setInt(3, activityid);
 	            ps2.executeUpdate();
 	            System.out.println("Successfully updated sport table");
 	        }
