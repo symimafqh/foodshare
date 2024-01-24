c56ea1a1af848825c9c15030453e84d555350fdc
symimafqh <2023380125@student.uitm.edu.my>
2024-01-24 22:34:54 +0800

    test API

---
+++ src/main/java/com/heroku/java/CONTROLLER/account/TeacherController.java
@@ -0,0 +1,42 @@
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.heroku.java.MODEL.teacher.TeacherBean;
+
+@Controller
+@RequestMapping("/api/teachers")
+public class TeacherController {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @GetMapping("/viewDetails")
+    public ResponseEntity<TeacherBean> viewTeacherDetails(@RequestParam("teacherUsername") String teacherUsername) {
+        try {
+            TeacherBean teacher = teacherRepository.getTeacherById(teacherUsername);
+
+            if (teacher != null) {
+                TeacherBean teacherBean = new TeacherBean();
+                teacherBean.setTeacherID(teacher.getTeacherID());
+                teacherBean.setTeacherUsername(teacher.getTeacherUsername());
+                teacherBean.setTeacherName(teacher.getTeacherName());
+                // Set other properties...
+
+                return new ResponseEntity<>(teacherBean, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
