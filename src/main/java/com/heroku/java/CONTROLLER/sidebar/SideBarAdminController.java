ffc85c8540eef26bef0faf3eb940b631b270d194
ZhahirahAli <2023189565@student.uitm.edu.my>
2024-05-13 00:34:02 +0800

    add student account

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarAdminController.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarAdminController.java
@@ -163,6 +163,51 @@ public class SideBarAdminController {
             return "redirect:/ListAccountTeacher";
     }
   //END OF ADD TEACHER ACCOUNT
+
+      //ADD STUDENT ACCOUNT
+      @PostMapping("/signup")
+      public String registerStudent(@ModelAttribute("signup")StudentBean s){
+          try {
+              Connection connection = dataSource.getConnection();
+              String sql = "INSERT INTO public.student(studentic, studentname, studentemail, studentphone, studentdob, studentgender, studentclass, studentaddress, studentpassword) VALUES(?,?,?,?,?,?,?,?,?)";
+              final var statement = connection.prepareStatement(sql);
+  
+              String studIC= s.getStudentIC();
+              String name= s.getStudentName();
+              String email=s.getStudentEmail();
+              String phone=s.getStudentPhone();
+              String dob=s.getStudentDOB();
+              String gender=s.getStudentGender();
+              String kelas=s.getStudentClass();
+              String address=s.getStudentAddress();
+              String password =s.getStudentPassword();
+      
+              statement.setString(1,studIC);
+              statement.setString(2,name);
+              statement.setString(3,email);
+              statement.setString(4,phone);
+              statement.setString(5,dob);
+              statement.setString(6,gender);
+              statement.setString(7,kelas);
+              statement.setString(8,address);
+              statement.setString(9,password);
+              
+              statement.executeUpdate();
+              
+              // System.out.println("type : "+protype);
+              // System.out.println("product price : RM"+proprice);
+              // System.out.println("proimg: "+proimgs.getBytes());
+              
+              connection.close();
+                  
+                  } catch (Exception e) {
+                      e.printStackTrace();
+                      return "redirect:/signup";
+                  }
+              return "redirect:/ListAccountStudent";
+      }
+  //END OF STUDENT ACCOUNT
+  
   // LIST ACCOUNT STUDENTS
    @GetMapping("/ListAccountStudent")
     public String studentAccountList(Model model) {
@@ -284,7 +329,7 @@ public class SideBarAdminController {
     return "redirect:/ListAccountStudent";
 }
 // END OF LIST ACCOUNT STUDENT
-  
+
 //LIST OF ACCOUNT TEACHER
 @GetMapping("/ListAccountTeacher")
 public String teacherAccountList(Model model) {
