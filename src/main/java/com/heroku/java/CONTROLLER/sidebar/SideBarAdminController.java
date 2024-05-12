77fd7aab4dd28a3f5581797fa5dda281a8e2453b
ZhahirahAli <2023189565@student.uitm.edu.my>
2024-05-12 23:40:42 +0800

    merge create account teacher and student controller into sidebar admin controller

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarAdminController.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarAdminController.java
@@ -117,6 +117,52 @@ public class SideBarAdminController {
       
   }
 
+  //ADD TEACHER ACCOUNT
+  @PostMapping("/teacherRegister")
+    public String registerTeacher(@ModelAttribute("teacherRegister")TeacherBean t){
+        try {
+            Connection connection = dataSource.getConnection();
+            String sql = "INSERT INTO public.teacher(teacherid, teachername, teacheremail, teacherphone, teacherdob, teacherrole, teachergender, teacheraddress, teacherusername, teacherpassword) VALUES(?,?,?,?,?,?,?,?,?,?)";
+            final var statement = connection.prepareStatement(sql);
+
+            String teacherid=t.getTeacherID();
+            String name= t.getTeacherName();
+            String email=t.getTeacherEmail();
+            String phone=t.getTeacherPhone();
+            String role=t.getTeacherRole();
+            String dob=t.getTeacherDOB();
+            String gender=t.getTeacherGender();
+            String username =t.getTeacherUsername();
+            String address=t.getTeacherAddress();
+            String password =t.getTeacherPassword();
+    
+            statement.setString(1,teacherid);
+            statement.setString(2,name);
+            statement.setString(3,email);
+            statement.setString(4,phone);
+            statement.setString(5,dob);
+            statement.setString(6,role);
+            statement.setString(7,gender);
+            statement.setString(8,address);
+            statement.setString(9,username);
+            statement.setString(10,password);
+            
+            statement.executeUpdate();
+            
+            // System.out.println("type : "+protype);
+            // System.out.println("product price : RM"+proprice);
+            // System.out.println("proimg: "+proimgs.getBytes());
+            
+            connection.close();
+                
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return "redirect:/teacherRegister";
+                }
+                
+            return "redirect:/ListAccountTeacher";
+    }
+  //END OF ADD TEACHER ACCOUNT
   // LIST ACCOUNT STUDENTS
    @GetMapping("/ListAccountStudent")
     public String studentAccountList(Model model) {
--- src/main/java/com/heroku/java/CONTROLLER/teacher/SignUpTeacher.java
+++ src/main/java/com/heroku/java/CONTROLLER/teacher/SignUpTeacher.java
@@ -1,77 +1,77 @@
-package com.heroku.java.CONTROLLER.teacher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+// package com.heroku.java.CONTROLLER.teacher;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-import com.heroku.java.MODEL.teacher.TeacherBean;
+// import com.heroku.java.MODEL.teacher.TeacherBean;
 
-import jakarta.servlet.http.HttpSession;
+// import jakarta.servlet.http.HttpSession;
 
-import java.sql.*;
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Map;
+// import java.sql.*;
+// import javax.sql.DataSource;
+// import java.util.ArrayList;
+// import java.util.Map;
 
-import java.util.List;
+// import java.util.List;
 
-@Controller
-public class SignUpTeacher {
-    private final DataSource dataSource;
+// @Controller
+// public class SignUpTeacher {
+//     private final DataSource dataSource;
 
-    @Autowired
-    public SignUpTeacher(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+//     @Autowired
+//     public SignUpTeacher(DataSource dataSource) {
+//         this.dataSource = dataSource;
+//     }
 
-    @PostMapping("/teacherRegister")
-    public String registerTeacher(@ModelAttribute("teacherRegister")TeacherBean t){
-        try {
-            Connection connection = dataSource.getConnection();
-            String sql = "INSERT INTO public.teacher(teacherid, teachername, teacheremail, teacherphone, teacherdob, teacherrole, teachergender, teacheraddress, teacherusername, teacherpassword) VALUES(?,?,?,?,?,?,?,?,?,?)";
-            final var statement = connection.prepareStatement(sql);
+//     @PostMapping("/teacherRegister")
+//     public String registerTeacher(@ModelAttribute("teacherRegister")TeacherBean t){
+//         try {
+//             Connection connection = dataSource.getConnection();
+//             String sql = "INSERT INTO public.teacher(teacherid, teachername, teacheremail, teacherphone, teacherdob, teacherrole, teachergender, teacheraddress, teacherusername, teacherpassword) VALUES(?,?,?,?,?,?,?,?,?,?)";
+//             final var statement = connection.prepareStatement(sql);
 
-            String teacherid=t.getTeacherID();
-            String name= t.getTeacherName();
-            String email=t.getTeacherEmail();
-            String phone=t.getTeacherPhone();
-            String role=t.getTeacherRole();
-            String dob=t.getTeacherDOB();
-            String gender=t.getTeacherGender();
-            String username =t.getTeacherUsername();
-            String address=t.getTeacherAddress();
-            String password =t.getTeacherPassword();
+//             String teacherid=t.getTeacherID();
+//             String name= t.getTeacherName();
+//             String email=t.getTeacherEmail();
+//             String phone=t.getTeacherPhone();
+//             String role=t.getTeacherRole();
+//             String dob=t.getTeacherDOB();
+//             String gender=t.getTeacherGender();
+//             String username =t.getTeacherUsername();
+//             String address=t.getTeacherAddress();
+//             String password =t.getTeacherPassword();
     
-            statement.setString(1,teacherid);
-            statement.setString(2,name);
-            statement.setString(3,email);
-            statement.setString(4,phone);
-            statement.setString(5,dob);
-            statement.setString(6,role);
-            statement.setString(7,gender);
-            statement.setString(8,address);
-            statement.setString(9,username);
-            statement.setString(10,password);
+//             statement.setString(1,teacherid);
+//             statement.setString(2,name);
+//             statement.setString(3,email);
+//             statement.setString(4,phone);
+//             statement.setString(5,dob);
+//             statement.setString(6,role);
+//             statement.setString(7,gender);
+//             statement.setString(8,address);
+//             statement.setString(9,username);
+//             statement.setString(10,password);
             
-            statement.executeUpdate();
+//             statement.executeUpdate();
             
-            // System.out.println("type : "+protype);
-            // System.out.println("product price : RM"+proprice);
-            // System.out.println("proimg: "+proimgs.getBytes());
+//             // System.out.println("type : "+protype);
+//             // System.out.println("product price : RM"+proprice);
+//             // System.out.println("proimg: "+proimgs.getBytes());
             
-            connection.close();
+//             connection.close();
                 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return "redirect:/teacherRegister";
-                }
+//                 } catch (Exception e) {
+//                     e.printStackTrace();
+//                     return "redirect:/teacherRegister";
+//                 }
                 
-            return "redirect:/teachersignin";
-    }
-}
+//             return "redirect:/ListAccountTeacher";
+//     }
+// }
