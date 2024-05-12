f5b8e2c3ad61cbdfd0082f734fc737d430b3255c
ZhahirahAli <2023189565@student.uitm.edu.my>
2024-05-12 17:17:51 +0800

    merge list of teacher account into one controller

--- src/main/java/com/heroku/java/CONTROLLER/account/ListTeacherAccountController.java
+++ src/main/java/com/heroku/java/CONTROLLER/account/ListTeacherAccountController.java
@@ -1,159 +1,159 @@
-package com.heroku.java.CONTROLLER.account;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+// package com.heroku.java.CONTROLLER.account;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-import com.heroku.java.MODEL.student.StudentBean;
-import com.heroku.java.MODEL.teacher.TeacherBean;
+// import com.heroku.java.MODEL.student.StudentBean;
+// import com.heroku.java.MODEL.teacher.TeacherBean;
 
-import jakarta.servlet.http.HttpSession;
+// import jakarta.servlet.http.HttpSession;
 
-import java.sql.*;
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Map;
+// import java.sql.*;
+// import javax.sql.DataSource;
+// import java.util.ArrayList;
+// import java.util.Map;
 
-import java.util.List;
+// import java.util.List;
 
-@Controller
-public class ListTeacherAccountController {
-    private final DataSource dataSource;
+// @Controller
+// public class ListTeacherAccountController {
+//     private final DataSource dataSource;
 
-    @Autowired
-    public ListTeacherAccountController(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+//     @Autowired
+//     public ListTeacherAccountController(DataSource dataSource) {
+//         this.dataSource = dataSource;
+//     }
 
-    @GetMapping("/ListAccountTeacher")
-    public String studentAccountList(Model model) {
+//     @GetMapping("/ListAccountTeacher")
+//     public String studentAccountList(Model model) {
 
-        List<TeacherBean> teacher = new ArrayList<TeacherBean>();
+//         List<TeacherBean> teacher = new ArrayList<TeacherBean>();
 
-        try (Connection connection = dataSource.getConnection()) {
-            String sql = "SELECT * FROM teacher order by teacherid";
-            final var statement = connection.prepareStatement(sql);
-            //statement.setString(1, "baker"); (syahir punya nih)
-            final var resultSet = statement.executeQuery();
+//         try (Connection connection = dataSource.getConnection()) {
+//             String sql = "SELECT * FROM teacher order by teacherid";
+//             final var statement = connection.prepareStatement(sql);
+//             //statement.setString(1, "baker"); (syahir punya nih)
+//             final var resultSet = statement.executeQuery();
            
 
-            while (resultSet.next()) {
-                String teacherID = resultSet.getString("teacherID");
-                String teacherUsername = resultSet.getString("teacherUsername");
-                String teacherName = resultSet.getString("teacherName");
-                String teacherEmail = resultSet.getString("teacherEmail");
-                String teacherPhone = resultSet.getString("teacherPhone");
-                String teacherDOB = resultSet.getString("teacherDOB");
-                String teacherGender = resultSet.getString("teacherGender");
-               String teacherRole = resultSet.getString("teacherRole");
-               String teacherAddress = resultSet.getString("teacherAddress");
-               String teacherPassword = resultSet.getString("teacherPassword");
+//             while (resultSet.next()) {
+//                 String teacherID = resultSet.getString("teacherID");
+//                 String teacherUsername = resultSet.getString("teacherUsername");
+//                 String teacherName = resultSet.getString("teacherName");
+//                 String teacherEmail = resultSet.getString("teacherEmail");
+//                 String teacherPhone = resultSet.getString("teacherPhone");
+//                 String teacherDOB = resultSet.getString("teacherDOB");
+//                 String teacherGender = resultSet.getString("teacherGender");
+//                String teacherRole = resultSet.getString("teacherRole");
+//                String teacherAddress = resultSet.getString("teacherAddress");
+//                String teacherPassword = resultSet.getString("teacherPassword");
                 
-                TeacherBean t = new TeacherBean();
-                t.setTeacherID(teacherID);
-                t.setTeacherUsername(teacherUsername);
-                t.setTeacherName(teacherName);
-                t.setTeacherEmail(teacherEmail);
-                t.setTeacherPhone(teacherPhone);
-                t.setTeacherGender(teacherGender);  
-                t.setTeacherRole(teacherRole);
-                t.setTeacherAddress(teacherAddress);
-                t.setTeacherPassword(teacherPassword); 
-                t.setTeacherDOB(teacherDOB);             
+//                 TeacherBean t = new TeacherBean();
+//                 t.setTeacherID(teacherID);
+//                 t.setTeacherUsername(teacherUsername);
+//                 t.setTeacherName(teacherName);
+//                 t.setTeacherEmail(teacherEmail);
+//                 t.setTeacherPhone(teacherPhone);
+//                 t.setTeacherGender(teacherGender);  
+//                 t.setTeacherRole(teacherRole);
+//                 t.setTeacherAddress(teacherAddress);
+//                 t.setTeacherPassword(teacherPassword); 
+//                 t.setTeacherDOB(teacherDOB);             
 
-                teacher.add(t);
-                model.addAttribute("teacher", teacher);
-                //model.addAttribute("isAdmin", staffsrole != null && staffsrole.equals("admin")); // Add isAdmin flag to the modelF (syahir punya gak)
+//                 teacher.add(t);
+//                 model.addAttribute("teacher", teacher);
+//                 //model.addAttribute("isAdmin", staffsrole != null && staffsrole.equals("admin")); // Add isAdmin flag to the modelF (syahir punya gak)
 
-            }
+//             }
 
-            connection.close();
+//             connection.close();
 
-        return "account/ListAccountTeacher";
-        } catch (SQLException e) {
-            e.printStackTrace();
-            // Handle the exception as desired (e.g., show an error message)
-            return "error";
-        }
+//         return "account/ListAccountTeacher";
+//         } catch (SQLException e) {
+//             e.printStackTrace();
+//             // Handle the exception as desired (e.g., show an error message)
+//             return "error";
+//         }
         
-    }
+//     }
 
-        @GetMapping("/viewTeacherDetails")
-    public String viewTeacherDetails(@RequestParam("teacherID") String teacherID, HttpSession session, Model model) {
-        //String teacherUsername = (String) session.getAttribute("teacherUsername");
-        System.out.println("ID Number : " + teacherID);
-        try {
-            Connection connection = dataSource.getConnection();
-            String sql = "SELECT * FROM public.teacher where teacherid=?";
-            final var statement = connection.prepareStatement(sql);
-            statement.setString(1, teacherID);
-            final var resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                String teacherUsername = resultSet.getString("teacherUsername");
-                String teacherName = resultSet.getString("teacherName");
-                String teacherEmail = resultSet.getString("teacherEmail");
-                String teacherPhone = resultSet.getString("teacherPhone");
-                String teacherDOB = resultSet.getString("teacherDOB");
-                String teacherGender = resultSet.getString("teacherGender");
-               String teacherRole = resultSet.getString("teacherRole");
-               String teacherAddress = resultSet.getString("teacherAddress");
-               String teacherPassword = resultSet.getString("teacherPassword");
+//         @GetMapping("/viewTeacherDetails")
+//     public String viewTeacherDetails(@RequestParam("teacherID") String teacherID, HttpSession session, Model model) {
+//         //String teacherUsername = (String) session.getAttribute("teacherUsername");
+//         System.out.println("ID Number : " + teacherID);
+//         try {
+//             Connection connection = dataSource.getConnection();
+//             String sql = "SELECT * FROM public.teacher where teacherid=?";
+//             final var statement = connection.prepareStatement(sql);
+//             statement.setString(1, teacherID);
+//             final var resultSet = statement.executeQuery();
+//             if (resultSet.next()) {
+//                 String teacherUsername = resultSet.getString("teacherUsername");
+//                 String teacherName = resultSet.getString("teacherName");
+//                 String teacherEmail = resultSet.getString("teacherEmail");
+//                 String teacherPhone = resultSet.getString("teacherPhone");
+//                 String teacherDOB = resultSet.getString("teacherDOB");
+//                 String teacherGender = resultSet.getString("teacherGender");
+//                String teacherRole = resultSet.getString("teacherRole");
+//                String teacherAddress = resultSet.getString("teacherAddress");
+//                String teacherPassword = resultSet.getString("teacherPassword");
 
-               TeacherBean t = new TeacherBean();
+//                TeacherBean t = new TeacherBean();
 
-               t.setTeacherID(teacherID);
-               t.setTeacherUsername(teacherUsername);
-               t.setTeacherName(teacherName);
-               t.setTeacherEmail(teacherEmail);
-               t.setTeacherPhone(teacherPhone);
-               t.setTeacherGender(teacherGender);  
-               t.setTeacherRole(teacherRole);
-               t.setTeacherAddress(teacherAddress);
-               t.setTeacherPassword(teacherPassword); 
-               t.setTeacherDOB(teacherDOB);   
+//                t.setTeacherID(teacherID);
+//                t.setTeacherUsername(teacherUsername);
+//                t.setTeacherName(teacherName);
+//                t.setTeacherEmail(teacherEmail);
+//                t.setTeacherPhone(teacherPhone);
+//                t.setTeacherGender(teacherGender);  
+//                t.setTeacherRole(teacherRole);
+//                t.setTeacherAddress(teacherAddress);
+//                t.setTeacherPassword(teacherPassword); 
+//                t.setTeacherDOB(teacherDOB);   
 
-                model.addAttribute("t", t);
+//                 model.addAttribute("t", t);
 
-                connection.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//                 connection.close();
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
-        return "account/viewTeacherDetails";
-    }
+//         return "account/viewTeacherDetails";
+//     }
 
-    @GetMapping("/DeleteTeacherAccount")
-    public String DeleteTeacherAccount(HttpSession session, Model model, @RequestParam("teacherID") String teacherID) {
+//     @GetMapping("/DeleteTeacherAccount")
+//     public String DeleteTeacherAccount(HttpSession session, Model model, @RequestParam("teacherID") String teacherID) {
 
-        String teacherUsername = (String) session.getAttribute("teacherUsername");
-        String teacherName = (String) session.getAttribute("teacherName");
-        String teacherEmail = (String) session.getAttribute("teacherEmail");
-        String teacherPhone = (String) session.getAttribute("teacherPhone");
-        String teacherDOB = (String) session.getAttribute("teacherDOB");
-        String teacherGender = (String) session.getAttribute("teacherGender");
-        String teacherClass = (String) session.getAttribute("teacherClass");
-        String teacherAddress = (String) session.getAttribute("teacherAddress");
-        String teacherPassword = (String) session.getAttribute("teacherPassword");
+//         String teacherUsername = (String) session.getAttribute("teacherUsername");
+//         String teacherName = (String) session.getAttribute("teacherName");
+//         String teacherEmail = (String) session.getAttribute("teacherEmail");
+//         String teacherPhone = (String) session.getAttribute("teacherPhone");
+//         String teacherDOB = (String) session.getAttribute("teacherDOB");
+//         String teacherGender = (String) session.getAttribute("teacherGender");
+//         String teacherClass = (String) session.getAttribute("teacherClass");
+//         String teacherAddress = (String) session.getAttribute("teacherAddress");
+//         String teacherPassword = (String) session.getAttribute("teacherPassword");
 
-    try (Connection connection = dataSource.getConnection()) {
-        // Delete student from the students account list
-        final var DeleteTeacherAccount = connection.prepareStatement("DELETE FROM public.teacher WHERE teacherid = ?");
-        DeleteTeacherAccount.setString(1, teacherID);
-        DeleteTeacherAccount.executeUpdate();
+//     try (Connection connection = dataSource.getConnection()) {
+//         // Delete student from the students account list
+//         final var DeleteTeacherAccount = connection.prepareStatement("DELETE FROM public.teacher WHERE teacherid = ?");
+//         DeleteTeacherAccount.setString(1, teacherID);
+//         DeleteTeacherAccount.executeUpdate();
 
-        System.out.println("Student account successfully deleted");
-    } catch (Exception e) {
-        System.out.println("Failed to delete teacher from the teacher account list");
-        e.printStackTrace();
-    }
-    return "redirect:/ListAccountTeacher";
-}
+//         System.out.println("Student account successfully deleted");
+//     } catch (Exception e) {
+//         System.out.println("Failed to delete teacher from the teacher account list");
+//         e.printStackTrace();
+//     }
+//     return "redirect:/ListAccountTeacher";
+// }
 
-}
+// }
--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarAdminController.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarAdminController.java
@@ -117,6 +117,7 @@ public class SideBarAdminController {
       
   }
 
+  // LIST ACCOUNT STUDENTS
    @GetMapping("/ListAccountStudent")
     public String studentAccountList(Model model) {
 
@@ -236,7 +237,132 @@ public class SideBarAdminController {
     }
     return "redirect:/ListAccountStudent";
 }
-
+// END OF LIST ACCOUNT STUDENT
   
+//LIST OF ACCOUNT TEACHER
+@GetMapping("/ListAccountTeacher")
+public String studentAccountList(Model model) {
 
+    List<TeacherBean> teacher = new ArrayList<TeacherBean>();
+
+    try (Connection connection = dataSource.getConnection()) {
+        String sql = "SELECT * FROM teacher order by teacherid";
+        final var statement = connection.prepareStatement(sql);
+        //statement.setString(1, "baker"); (syahir punya nih)
+        final var resultSet = statement.executeQuery();
+       
+
+        while (resultSet.next()) {
+            String teacherID = resultSet.getString("teacherID");
+            String teacherUsername = resultSet.getString("teacherUsername");
+            String teacherName = resultSet.getString("teacherName");
+            String teacherEmail = resultSet.getString("teacherEmail");
+            String teacherPhone = resultSet.getString("teacherPhone");
+            String teacherDOB = resultSet.getString("teacherDOB");
+            String teacherGender = resultSet.getString("teacherGender");
+           String teacherRole = resultSet.getString("teacherRole");
+           String teacherAddress = resultSet.getString("teacherAddress");
+           String teacherPassword = resultSet.getString("teacherPassword");
+            
+            TeacherBean t = new TeacherBean();
+            t.setTeacherID(teacherID);
+            t.setTeacherUsername(teacherUsername);
+            t.setTeacherName(teacherName);
+            t.setTeacherEmail(teacherEmail);
+            t.setTeacherPhone(teacherPhone);
+            t.setTeacherGender(teacherGender);  
+            t.setTeacherRole(teacherRole);
+            t.setTeacherAddress(teacherAddress);
+            t.setTeacherPassword(teacherPassword); 
+            t.setTeacherDOB(teacherDOB);             
+
+            teacher.add(t);
+            model.addAttribute("teacher", teacher);
+            //model.addAttribute("isAdmin", staffsrole != null && staffsrole.equals("admin")); // Add isAdmin flag to the modelF (syahir punya gak)
+
+        }
+
+        connection.close();
+
+    return "account/ListAccountTeacher";
+    } catch (SQLException e) {
+        e.printStackTrace();
+        // Handle the exception as desired (e.g., show an error message)
+        return "error";
+    }
+    
+}
+
+    @GetMapping("/viewTeacherDetails")
+public String viewTeacherDetails(@RequestParam("teacherID") String teacherID, HttpSession session, Model model) {
+    //String teacherUsername = (String) session.getAttribute("teacherUsername");
+    System.out.println("ID Number : " + teacherID);
+    try {
+        Connection connection = dataSource.getConnection();
+        String sql = "SELECT * FROM public.teacher where teacherid=?";
+        final var statement = connection.prepareStatement(sql);
+        statement.setString(1, teacherID);
+        final var resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            String teacherUsername = resultSet.getString("teacherUsername");
+            String teacherName = resultSet.getString("teacherName");
+            String teacherEmail = resultSet.getString("teacherEmail");
+            String teacherPhone = resultSet.getString("teacherPhone");
+            String teacherDOB = resultSet.getString("teacherDOB");
+            String teacherGender = resultSet.getString("teacherGender");
+           String teacherRole = resultSet.getString("teacherRole");
+           String teacherAddress = resultSet.getString("teacherAddress");
+           String teacherPassword = resultSet.getString("teacherPassword");
+
+           TeacherBean t = new TeacherBean();
+
+           t.setTeacherID(teacherID);
+           t.setTeacherUsername(teacherUsername);
+           t.setTeacherName(teacherName);
+           t.setTeacherEmail(teacherEmail);
+           t.setTeacherPhone(teacherPhone);
+           t.setTeacherGender(teacherGender);  
+           t.setTeacherRole(teacherRole);
+           t.setTeacherAddress(teacherAddress);
+           t.setTeacherPassword(teacherPassword); 
+           t.setTeacherDOB(teacherDOB);   
+
+            model.addAttribute("t", t);
+
+            connection.close();
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    return "account/viewTeacherDetails";
+}
+
+@GetMapping("/DeleteTeacherAccount")
+public String DeleteTeacherAccount(HttpSession session, Model model, @RequestParam("teacherID") String teacherID) {
+
+    String teacherUsername = (String) session.getAttribute("teacherUsername");
+    String teacherName = (String) session.getAttribute("teacherName");
+    String teacherEmail = (String) session.getAttribute("teacherEmail");
+    String teacherPhone = (String) session.getAttribute("teacherPhone");
+    String teacherDOB = (String) session.getAttribute("teacherDOB");
+    String teacherGender = (String) session.getAttribute("teacherGender");
+    String teacherClass = (String) session.getAttribute("teacherClass");
+    String teacherAddress = (String) session.getAttribute("teacherAddress");
+    String teacherPassword = (String) session.getAttribute("teacherPassword");
+
+try (Connection connection = dataSource.getConnection()) {
+    // Delete student from the students account list
+    final var DeleteTeacherAccount = connection.prepareStatement("DELETE FROM public.teacher WHERE teacherid = ?");
+    DeleteTeacherAccount.setString(1, teacherID);
+    DeleteTeacherAccount.executeUpdate();
+
+    System.out.println("Student account successfully deleted");
+} catch (Exception e) {
+    System.out.println("Failed to delete teacher from the teacher account list");
+    e.printStackTrace();
+}
+return "redirect:/ListAccountTeacher";
+}
+//END OF LIST ACCOUNT TEACHER
 }
