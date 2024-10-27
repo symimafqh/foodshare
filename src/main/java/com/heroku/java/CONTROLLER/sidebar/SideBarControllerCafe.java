56e918316280a327da7db02278247ddc6f60933f
symimafqh <2023380125@student.uitm.edu.my>
2024-10-27 17:59:45 +0800

    BETULKAN MAPP

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerCafe.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerCafe.java
@@ -100,7 +100,7 @@ public class SideBarControllerCafe {
             e.printStackTrace();
         }
 
-        return "cafe/profileCafe/profileCafe_edit";
+        return "cafeteria_owner/profileCO/profileCafe_edit";
     }
 
 }
