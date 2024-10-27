050c5d14086f65aebe5cd8b16d06de562cbb9c2c
symimafqh <2023380125@student.uitm.edu.my>
2024-10-27 14:38:16 +0800

    mapping

--- src/main/java/com/heroku/java/CONTROLLER/cafeteria_owner/LoginCafeteriaOwnerController.java
+++ src/main/java/com/heroku/java/CONTROLLER/cafeteria_owner/LoginCafeteriaOwnerController.java
@@ -35,7 +35,7 @@ public class LoginCafeteriaOwnerController {
 
     @GetMapping("/cafesignin")
     public String cafeteriaSignin() {
-        return "cafeteria_owner/sign-in/cafesignin";
+        return "cafeteria_owner/sign-in/ownerSignIn";
     }
 
     @PostMapping("/cafesignin")
