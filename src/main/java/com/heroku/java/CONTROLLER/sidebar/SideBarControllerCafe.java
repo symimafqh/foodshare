fb29a6378fdd5ddd5bacc87cffba4eef42608ebd
symimafqh <2023380125@student.uitm.edu.my>
2024-10-27 22:56:40 +0800

    BETULKAN NAMING

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerCafe.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerCafe.java
@@ -102,7 +102,7 @@ public class SideBarControllerCafe {
     }
 
     // Get Mapping for Add Leftover Page
-    @GetMapping("/add_leftover")
+    @GetMapping("/addLeftover")
     public String leftoverAdd(@RequestParam(name = "success", required = false) Boolean success, Model model,
             HttpSession session) {
         String cafeNumber = (String) session.getAttribute("cafeNumber");
