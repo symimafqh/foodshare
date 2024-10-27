45704887dc7d7791ad9f0b9ed7d87494ebbc8a09
symimafqh <2023380125@student.uitm.edu.my>
2024-10-27 17:28:34 +0800

    tukar nama table

--- src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerCafe.java
+++ src/main/java/com/heroku/java/CONTROLLER/sidebar/SideBarControllerCafe.java
@@ -70,7 +70,7 @@ public class SideBarControllerCafe {
         String cafeNumber = (String) session.getAttribute("cafeNumber");
         try {
             Connection connection = dataSource.getConnection();
-            String sql = "SELECT * FROM public.cafe where cafeNumber=?";
+            String sql = "SELECT * FROM public.cafeteria_owner where cafeNumber=?";
             final var statement = connection.prepareStatement(sql);
             statement.setString(1, cafeNumber);
             final var resultSet = statement.executeQuery();
