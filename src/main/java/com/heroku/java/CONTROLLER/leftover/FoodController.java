5b5b8227a3845348c856445757e41e94045abe3b
symimafqh <2023380125@student.uitm.edu.my>
2024-11-03 23:21:55 +0800

    salah eja dek

--- src/main/java/com/heroku/java/CONTROLLER/leftover/FoodController.java
+++ src/main/java/com/heroku/java/CONTROLLER/leftover/FoodController.java
@@ -44,7 +44,7 @@ public class FoodController {
             return "redirect:/error"; // Handle errors gracefully
         }
         model.addAttribute("foodList", foodList); // Add the food list to the model
-        return "cafteria_owner/leftover/foodList"; // Return the view name
+        return "cafeteria_owner/leftover/foodList"; // Return the view name
     }
 
     // Method to get specific food details for updating
