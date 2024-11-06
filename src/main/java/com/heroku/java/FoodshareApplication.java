8662912e083bcf13535e34184b25aca792f6db5d
symimafqh <2023380125@student.uitm.edu.my>
2024-11-06 23:07:40 +0800

    try

---
+++ src/main/java/com/heroku/java/FoodshareApplication.java
@@ -0,0 +1,14 @@
+package com.heroku.java; // Ensure this matches your package structure
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodshareApplication { // You can name this class as needed
+
+    public static void main(String[] args) {
+        System.setProperty("https.protocols", "TLSv1.2,TLSv1.3");
+
+        SpringApplication.run(FoodshareApplication.class, args); // This line starts your application
+    }
+}
