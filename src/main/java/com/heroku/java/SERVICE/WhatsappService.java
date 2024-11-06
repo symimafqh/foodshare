dfd2a02972f4a990d7ede77dfe994964d09ceb6c
symimafqh <2023380125@student.uitm.edu.my>
2024-11-06 23:17:54 +0800

    notification

--- src/main/java/com/heroku/java/SERVICE/WhatsappService.java
+++ src/main/java/com/heroku/java/SERVICE/WhatsappService.java
@@ -1,5 +1,6 @@
 package com.heroku.java.SERVICE;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
@@ -10,6 +11,10 @@ import org.json.JSONObject;
 @Service
 public class WhatsappService {
     private final String apiUrl = "https://whatsapp.kwlabs.xyz/api/sendText";
+
+    // Inject the API key from environment variables
+    @Value("${WHATSAPP_API_KEY}")
+    private String apiKey;
 
     public String sendMessage(String chatId, String message) {
         JSONObject payload = new JSONObject();
@@ -22,6 +27,7 @@ public class WhatsappService {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/json");
         headers.set("Content-Type", "application/json");
+        headers.set("Authorization", "Bearer " + apiKey); // Include the API key in the headers
         HttpEntity<String> entity = new HttpEntity<>(payload.toString(), headers);
 
         ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, entity, String.class);
