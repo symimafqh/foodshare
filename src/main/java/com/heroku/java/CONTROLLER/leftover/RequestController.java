6c83f52d625f3bcf17349b17d11b7d844896cb83
symimafqh <2023380125@student.uitm.edu.my>
2024-11-10 11:28:49 +0800

    asdad

--- src/main/java/com/heroku/java/CONTROLLER/leftover/RequestController.java
+++ src/main/java/com/heroku/java/CONTROLLER/leftover/RequestController.java
@@ -82,10 +82,7 @@ public class RequestController {
     }
 
     @PostMapping("/request_leftover")
-    public String requestLeftover(Model model, HttpSession session, LeftoverBean leftover, @RequestParam("foodid") int foodId,
-    @RequestParam("foodname") String foodName,
-    @RequestParam("foodquantity") int foodQuantity,
-    @RequestParam("fooddescription") String foodDescription) {
+    public String requestLeftover(Model model, HttpSession session, LeftoverBean leftover, @RequestParam("foodid") int foodId) {
         String studentNumber = (String) session.getAttribute("studentNumber");
         String status = "Pending";
     
