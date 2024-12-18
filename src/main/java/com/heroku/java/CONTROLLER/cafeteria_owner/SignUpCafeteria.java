4552dae22add698c66e97acb96333d183f4a3d38
symimafqh <2023380125@student.uitm.edu.my>
2024-12-18 08:59:05 +0800

    test register

--- src/main/java/com/heroku/java/CONTROLLER/cafeteria_owner/SignUpCafeteria.java
+++ src/main/java/com/heroku/java/CONTROLLER/cafeteria_owner/SignUpCafeteria.java
@@ -34,7 +34,7 @@ public class SignUpCafeteria {
     public String registerCafe(@ModelAttribute("cafeRegister")CafeBean t){
         try {
             Connection connection = dataSource.getConnection();
-            String sql = "INSERT INTO public.cafeteria_owner(\"cafeNumber\", \"cafeName\", \"cafeEmail\",\"phoneNumber\", \"cafePassword\") VALUES(?,?,?,?)";
+            String sql = "INSERT INTO public.cafeteria_owner(\"cafeNumber\", \"cafeName\", \"cafeEmail\",\"phoneNumber\", \"cafePassword\") VALUES(?,?,?,?,?)";
             final var statement = connection.prepareStatement(sql);
 
             String name= t.getCafeName();
