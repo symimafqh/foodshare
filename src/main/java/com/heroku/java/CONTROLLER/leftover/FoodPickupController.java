6e05d9b689e643f42d04a9097d05c539e7a09a04
symimafqh <2023380125@student.uitm.edu.my>
2024-12-28 19:04:20 +0800

    controller tinggal pulok

--- src/main/java/com/heroku/java/CONTROLLER/leftover/FoodPickupController.java
+++ src/main/java/com/heroku/java/CONTROLLER/leftover/FoodPickupController.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,8 @@ import com.heroku.java.MODEL.student.StudentBean;
 
 import jakarta.servlet.http.HttpSession;
 
+
+@Controller
 public class FoodPickupController {
 
     @Autowired
